09e9bc7504c76033e07821e2d585c668d60fd5fa
Naoya Fujimoto <soccer.ikimono.711@gmail.com>
2022-05-11 15:09:27 +0900

    20220511_15:11 画像ファイルの追加とheader.phpの追加

---
+++ inc/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <!-- 文字コード -->
+    <meta charset="UTF-8">
+    <!-- IEバグ回避用 -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- 説明文 -->
+    <meta name="description" content="">
+    <!-- ビューポート -->
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <!-- OGP -->
+    <meta property="og:title" content="">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:image" content="">
+    <!-- Twitter OGP -->
+    <meta name="twitter:card" content="summary_large_image">
+    <!-- ファビコン -->
+    <link rel="" href="">
+    <!-- Webフォント -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- アイコンフォント -->
+    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@2.0.2/destyle.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://use.typekit.net/qgw0lcp.css">
+</head>
+
+<body>
+    
+<!-- ヘッダー -->
+<header class="header">
+    <div class="header_inner">
+        <div class="header_logo"></div>
+
+        <nav class="header_nav">
+            <ul class="header_list">
+                <li><a href=""></a>法人様向けプラン</li>
+                <li><a href=""></a>個人様向けプラン</li>
+                <li><a href=""></a>利用者紹介</li>
+                <li><a href=""></a>アクセス</li>
+            </ul>
+
+            <a href="" class="button">内覧予約</a>
+        </nav>
+    </div>
+</header>
