23e88fe626511a3c8d982b56a77bf622e6b28e54
Naoya Fujimoto <soccer.ikimono.711@gmail.com>
2022-05-11 22:05:09 +0900

    20220511_22:05 footer.phpの追加

---
+++ inc/footer.php
@@ -0,0 +1,39 @@
+<!-- コンタクト -->
+<div class="contact">
+    <div class="contact_inner">
+        <a href="" class="button">今すぐ内覧予約</a>
+        <a href="" class="button">お問い合わせ</a>
+    </div>
+</div>
+
+<!-- フッター -->
+<footer class="footer">
+    <div class="footer_inner wrapper">
+        <img src="./image/logo-white.png" alt="ロゴの画像" class="footer_image">
+        <div class="footer_info">
+            <p class="address">
+                〒542-0076 
+                <br>大阪市中央区難波5丁目1−60 11F
+            </p>
+            <p class="tel_number">TEL：06-0000-1234</p>
+            <p class="business_day">平日・土日祝 9:00～22:00</p>
+        </div>
+        <div class="footer_nav">
+            <ul class="footer_list">
+                <li><a href=""></a>法人様向けプラン</li>
+                <li><a href=""></a>個人様向けプラン</li>
+                <li><a href=""></a>利用者紹介</li>
+                <li><a href=""></a>アクセス</li>
+            </ul>
+        </div>
+
+    </div>
+    <p class="copylight">@2022 THE PLACE NAMBA</p>
+</footer>
+
+
+<!-- JavaScript -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="./js/script.js"></script>
+
+</body>
