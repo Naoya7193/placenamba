fcbfc433be8fb971c77e415b0285411351e99a08
Naoya Fujimoto <soccer.ikimono.711@gmail.com>
2022-05-23 14:08:31 +0900

    20220523_14:07 固定ページ用のmv-temp.scssの追加/固定ページ用のwrapperの追加

--- inc/mv-temp.php
+++ inc/mv-temp.php
@@ -4,7 +4,11 @@ $title = get_the_title();
 ?>
 
 <div class="mv_temp">
-    <div class="mv_temp_title"><?= $title ?></div>
+    <div class="mv_temp_title">
+        <div class="mv_temp_title_inner">
+            <?= $title ?>
+        </div>
+    </div>
     <img src="<?php the_post_thumbnail_url() ?>" class="mv_temp_image">
 
 </div>
