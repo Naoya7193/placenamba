dd07f5befbe69e02a6e868b7b89f069357f12f47
Naoya Fujimoto <soccer.ikimono.711@gmail.com>
2022-05-24 14:36:36 +0900

    20220524_14:35 facilityブロックを固定ページ用のfacility-temp.scssに移動 / 対応するphp / scssファイルの追加

---
+++ inc/facility-temp.php
@@ -0,0 +1,32 @@
+<!-- 施設・設備 -->
+<div class="page-facility wrapper_sm">
+    <h2 class="page-section_main_title">Facility</h2>
+    <h3 class="page-section_sub_title">施設・設備</h3>
+
+    <div class="page-facility_container">
+        <div class="page-facility_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/facility01.png" alt="QR認証によるセキュリティ入退館の画像" class="page-facility_image">
+            <p class="page-facility_image_title">QR認証によるセキュリティ入退館</p>
+        </div>
+        <div class="page-facility_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/facility02.png" alt="デスク・チェア備え付きの画像" class="page-facility_image">
+            <p class="page-facility_image_title">デスク・チェア備え付き</p>
+        </div>
+        <div class="page-facility_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/facility03.png" alt="会議室の画像" class="page-facility_image">
+            <p class="page-facility_image_title">会議室</p>
+        </div>
+        <div class="page-facility_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/facility04.png" alt="防犯カメラ設置の画像" class="page-facility_image">
+            <p class="page-facility_image_title">防犯カメラ設置</p>
+        </div>
+        <div class="page-facility_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/facility05.png" alt="ゲスト同伴利用の画像" class="page-facility_image">
+            <p class="page-facility_image_title">ゲスト同伴利用</p>
+        </div>
+        <div class="page-facility_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/facility06.png" alt="個別ロッカーの画像" class="page-facility_image">
+            <p class="page-facility_image_title">個別ロッカー</p>
+        </div>
+    </div>
+</div>
--- page-corporate.php
+++ page-corporate.php
@@ -27,40 +27,9 @@
             </tr>
         </table>
     </div>
-
-    <!-- 施設・設備 -->
-    <div class="page-corporate_facility">
-        <h2 class="page-section_main_title">Facility</h2>
-        <h3 class="page-section_sub_title">施設・設備</h3>
-
-        <div class="page-corporate_facility_container">
-            <div class="page-corporate_facility_card">
-                <img src="<?php echo get_template_directory_uri(); ?>/image/facility01.png" alt="QR認証によるセキュリティ入退館の画像" class="page-corporate_facility_image">
-                <p class="page-corporate_facility_image_title">QR認証によるセキュリティ入退館</p>
-            </div>
-            <div class="page-corporate_facility_card">
-                <img src="<?php echo get_template_directory_uri(); ?>/image/facility02.png" alt="デスク・チェア備え付きの画像" class="page-corporate_facility_image">
-                <p class="page-corporate_facility_image_title">デスク・チェア備え付き</p>
-            </div>
-            <div class="page-corporate_facility_card">
-                <img src="<?php echo get_template_directory_uri(); ?>/image/facility03.png" alt="会議室の画像" class="page-corporate_facility_image">
-                <p class="page-corporate_facility_image_title">会議室</p>
-            </div>
-            <div class="page-corporate_facility_card">
-                <img src="<?php echo get_template_directory_uri(); ?>/image/facility04.png" alt="防犯カメラ設置の画像" class="page-corporate_facility_image">
-                <p class="page-corporate_facility_image_title">防犯カメラ設置</p>
-            </div>
-            <div class="page-corporate_facility_card">
-                <img src="<?php echo get_template_directory_uri(); ?>/image/facility05.png" alt="ゲスト同伴利用の画像" class="page-corporate_facility_image">
-                <p class="page-corporate_facility_image_title">ゲスト同伴利用</p>
-            </div>
-            <div class="page-corporate_facility_card">
-                <img src="<?php echo get_template_directory_uri(); ?>/image/facility06.png" alt="個別ロッカーの画像" class="page-corporate_facility_image">
-                <p class="page-corporate_facility_image_title">個別ロッカー</p>
-            </div>
-        </div>
-    </div>
 </div>
+
+<?php include 'inc/facility-temp.php'; ?>
 
 <?php include 'inc/others-temp.php'; ?>
 
