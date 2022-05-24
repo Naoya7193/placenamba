3e95335136f0c71f28868b12105c709c75a0f919
Naoya Fujimoto <soccer.ikimono.711@gmail.com>
2022-05-24 14:18:43 +0900

    20220524_14:16 固定ページ用のothers-temp.scssの追加 / corporateページのphp / scssファイルの追加

---
+++ inc/others-temp.php
@@ -0,0 +1,32 @@
+<!-- その他サービス -->
+<div class="page-others wrapper_sm">
+    <h2 class="page-section_main_title">Others</h2>
+    <h3 class="page-section_sub_title">その他サービス</h3>
+
+    <div class="service_container">
+        <div class="service_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/wifi.png" alt="wifiのアイコン" class="service_image">
+            <p class="service_title">Wi-fi</p>
+        </div>
+        <div class="service_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/cost.png" alt="光熱費のアイコン" class="service_image">
+            <p class="service_title">水道・光熱費込</p>
+        </div>
+        <div class="service_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/reception.png" alt="受付のアイコン" class="service_image">
+            <p class="service_title">受付対応</p>
+        </div>
+        <div class="service_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/load.png" alt="荷物受取のアイコン" class="service_image">
+            <p class="service_title">荷物受取</p>
+        </div>
+        <div class="service_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/airconditioning.png" alt="空調のアイコン" class="service_image">
+            <p class="service_title">空調</p>
+        </div>
+        <div class="service_card">
+            <img src="<?php echo get_template_directory_uri(); ?>/image/drink.png" alt="ドリンクサービスのアイコン" class="service_image">
+            <p class="service_title">ドリンクサービス</p>
+        </div>
+    </div>
+</div>
--- page-corporate.php
+++ page-corporate.php
@@ -2,5 +2,66 @@
 
 <?php include 'inc/mv-temp.php'; ?>
 
+<!-- 企業様向けプラン -->
+<div class="page-corporate wrapper_sm">
+    <p class="page-corporate_copy">企業様向けプライベートオフィスプラン</p>
+    <div class="page-corporate_text">
+        <p class="page-corporate_text_inner">
+            プライベートオフィスの説明をする。文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れていまこの文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れていま
+        </p>
+    </div>
+
+    <!-- 料金 -->
+    <div class="page-corporate_price">
+        <h2 class="page-section_main_title">Price</h2>
+        <h3 class="page-section_sub_title">料金</h3>
+
+        <table class="page-corporate_table">
+            <tr>
+                <td>月額</td>
+                <td>お問い合わせください</td>
+            </tr>
+            <tr>
+                <td>初期費用</td>
+                <td>月額賃料2ヶ月分程度</td>
+            </tr>
+        </table>
+    </div>
+
+    <!-- 施設・設備 -->
+    <div class="page-corporate_facility">
+        <h2 class="page-section_main_title">Facility</h2>
+        <h3 class="page-section_sub_title">施設・設備</h3>
+
+        <div class="page-corporate_facility_container">
+            <div class="page-corporate_facility_card">
+                <img src="<?php echo get_template_directory_uri(); ?>/image/facility01.png" alt="QR認証によるセキュリティ入退館の画像" class="page-corporate_facility_image">
+                <p class="page-corporate_facility_image_title">QR認証によるセキュリティ入退館</p>
+            </div>
+            <div class="page-corporate_facility_card">
+                <img src="<?php echo get_template_directory_uri(); ?>/image/facility02.png" alt="デスク・チェア備え付きの画像" class="page-corporate_facility_image">
+                <p class="page-corporate_facility_image_title">デスク・チェア備え付き</p>
+            </div>
+            <div class="page-corporate_facility_card">
+                <img src="<?php echo get_template_directory_uri(); ?>/image/facility03.png" alt="会議室の画像" class="page-corporate_facility_image">
+                <p class="page-corporate_facility_image_title">会議室</p>
+            </div>
+            <div class="page-corporate_facility_card">
+                <img src="<?php echo get_template_directory_uri(); ?>/image/facility04.png" alt="防犯カメラ設置の画像" class="page-corporate_facility_image">
+                <p class="page-corporate_facility_image_title">防犯カメラ設置</p>
+            </div>
+            <div class="page-corporate_facility_card">
+                <img src="<?php echo get_template_directory_uri(); ?>/image/facility05.png" alt="ゲスト同伴利用の画像" class="page-corporate_facility_image">
+                <p class="page-corporate_facility_image_title">ゲスト同伴利用</p>
+            </div>
+            <div class="page-corporate_facility_card">
+                <img src="<?php echo get_template_directory_uri(); ?>/image/facility06.png" alt="個別ロッカーの画像" class="page-corporate_facility_image">
+                <p class="page-corporate_facility_image_title">個別ロッカー</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'inc/others-temp.php'; ?>
 
 <?php include 'inc/footer.php'; ?>
