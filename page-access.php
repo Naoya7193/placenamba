5dc8cd18432de9027a85e62092aa0be5b5701337
Naoya Fujimoto <soccer.ikimono.711@gmail.com>
2022-05-24 12:32:16 +0900

    20220524_12:30 accessページのphp / scssファイルの追加

--- page-access.php
+++ page-access.php
@@ -1,5 +1,15 @@
+<!-- 変数処理 -->
+<?php
+$content = get_the_content();
+?>
+
 <?php include 'inc/header.php'; ?>
 
 <?php include 'inc/mv-temp.php'; ?>
+
+<!-- アクセス -->
+<div class="page-access wrapper_sm">
+    <?=$content?>
+</div>
 
 <?php include 'inc/footer.php'; ?>
